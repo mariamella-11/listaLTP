94c2a04d5612154097bd96fb4db9c20414a2961b
Maria Mella <maria.mella@estudante.ifms.edu.br>
2026-05-11 09:52:49 -0400

    switch: case 1

--- main.js
+++ main.js
@@ -3,3 +3,37 @@ const Livro = require('./Livro') ;
 
 const livros = []
 
+opcao = null;
+
+while (opcao != 0){
+    console.log("--- BIBLIOTECA ---");
+    console.log("1 -  Cadastrar livro");
+    console.log("2 - Listar livros");
+    console.log("3 - Alterar livro");
+    console.log("4 - Estatísticas");
+    console.log("0 - Sair");
+}
+
+opcao = parseInt(prompt("opcao:"));
+
+if(isNaN(opcao)) {
+    console.log("digite um numero valido");
+    
+}
+
+switch (opcao) {
+    case 1: 
+    let titulo = prompt("titulo: ")
+    let autor = prompt("autor: ");
+    let genero = prompt("genero: ");
+    let anoPubli = parseInt(prompt("ano de publicacao: "));
+    let numPaginas = parseInt(prompt("numero de paginas: "));
+
+    let novoLivro = new Livro(titulo, autor, genero, anoPubli, numPaginas);
+    livros.push(novoLivro);
+    console.log("Livro cadastrado");
+    break;
+
+    case 2:
+
+}
