77279e60baf89179344ffad6b50e4e04a5a55a06
Maria Mella <maria.mella@estudante.ifms.edu.br>
2026-05-11 19:29:16 -0400

    switch: case 4 e 0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Livro = require('./Livro') ;
 
 const livros = []
 
-opcao = null;
+let opcao = null;
 
 while (opcao != 0){
     console.log("--- BIBLIOTECA ---");
@@ -12,84 +12,119 @@ while (opcao != 0){
     console.log("3 - Alterar livro");
     console.log("4 - Estatísticas");
     console.log("0 - Sair");
-}
 
-opcao = parseInt(prompt("opcao:"));
+    opcao = parseInt(prompt("opcao:"));
 
-if(isNaN(opcao)) {
+
+
+    if(isNaN(opcao)) {
     console.log("digite um numero valido");
-    
-}
+    continue;
+    }
 
-switch (opcao) {
-    case 1: 
-    let titulo = prompt("titulo: ")
-    let autor = prompt("autor: ");
-    let genero = prompt("genero: ");
-    let anoPubli = parseInt(prompt("ano de publicacao: "));
-    let numPaginas = parseInt(prompt("numero de paginas: "));
+    switch (opcao) {
+        case 1: 
+            let titulo = prompt("titulo: ")
+            let autor = prompt("autor: ");
+            let genero = prompt("genero: ");
+            let anoPub = parseInt(prompt("ano de publicacao: "));
+            let numPaginas = parseInt(prompt("numero de paginas: "));
 
-    let novoLivro = new Livro(titulo, autor, genero, anoPubli, numPaginas);
-    livros.push(novoLivro);
-    console.log("Livro cadastrado");
-    break;
+            let novoLivro = new Livro(titulo, autor, genero, anoPub, numPaginas);
+            livros.push(novoLivro);
+            console.log("Livro cadastrado");
+            break;
 
-    case 2:
-        if(livros.length === 0){
-            console.log("nenhum livro cadastrado")
+        case 2:
+            if(livros.length === 0){
+                console.log("nenhum livro cadastrado")
 
-        }
-        else{
+            }
+            else{
+                livros.forEach((livro, indice) => {
+                    console.log(`[${indice}] ${livro.getInfo()}`);
+            })
+            }
+            break;
+
+        case 3:
+            if (livros.length === 0){
+                console.log("nenhum livro cadastrado");
+                break
+            }
+            console.log("\nlivros disponiveis: ");
+
             livros.forEach((livro, indice) => {
-                console.log(`[${indice}] ${livro.getInfo()}`)});
+                console.log(`[${indice}] ${livro.getInfo()}`)
+            })
+            let indice = parseInt(prompt("digite o indice do livro: "));
+            if(
+                isNaN(indice)||
+                indice<0||
+                indice>=livros.length
+            ){
+                console.log("indice invalido!")
+                    break;
+            }
+            let livro = livros[indice];
+
+            let novoTitulo = prompt(`titulo (${livro.titulo}): `)
+
+            if (novoTitulo.trim() !== ""){  
+                livro.titulo = novoTitulo;
+            }
+
+            let novoAutor = prompt(`Autor (${livro.autor}): `)
+
+            if (novoAutor.trim() !== ""){  
+                livro.autor = novoAutor;
+            }
+
+            let novoGenero = prompt(`Genero (${livro.genero}): `)
+
+            if (novoGenero.trim() !== ""){  
+                livro.genero = novoGenero;
+            }
+
+        let novoAno = prompt(`Ano (${livro.anoPub}): `)
+
+        if (novoAno.trim() !== ""){  
+            livro.anoPub = parseInt(novoAno);
         }
+
+        let novasPaginas = prompt(`Paginas (${livro.numPaginas}): `)
+
+        if (novasPaginas.trim() !== ""){  
+            livro.numPaginas = parseInt(novasPaginas);
+        }
+
+        console.log("Livro alterado!");
         break;
 
-    case 3:
-        if (livros.length === 0){
-            console.log("nenhum livro cadastrado");
-            break
+      case 4:
+        if (livros.length === 0) {
+            console.log("Nenhum livro cadastrado!");
+            break;
         }
-        console.log("\nlivros disponiveis: ");
-        livros.forEach((livro, indice) => {
-            console.log(`[${indice}] ${livro.getInfo}`)
-        })
-        let indice = parseInt(prompt("digite o indice do livro: "))
-      if(
-        isNaN(indice)||
-        indice<0||
-        indice>=livros.length
-      ){
-        console.log("indice invalido!")
+        let auxiliar = new Livro();
+        let generos = auxiliar.contarPorGenero(livros);
+        console.log("\nQuantidade por gênero: ")
+
+        for (let genero in generos){
+            console.log(`${genero}: ${generos[genero]}`);
+        }
+
+        let media = Number(auxiliar.mediaNumPaginas(livros))
+
+        console.log(`\nMedia de paginas: ${media.toFixed(2)}`);
+        
         break;
-      }
-      let livro = livros[indice];
-      let novoTitulo = prompt(`titulo (${livro.titulo}): `)
-      if (novoTitulo.trim() !== ""){  
-        livro.titulo = novoTitulo;
-      }
 
-      let novoAutor = prompt(`Autor (${livro.autor}): `)
-      if (novoAutor.trim() !== ""){  
-        livro.autor = novoAutor;
-      }
+        case 0:
+        console.log("programa encerrado");
+        break;
 
-      let novoGenero = prompt(`Genero (${livro.autor}): `)
-      if (novoGenero.trim() !== ""){  
-        livro.genero = novoGenero;
-      }
-
-      let novoAno = prompt(`Ano (${livro.anoPubli}): `)
-      if (novoTitulo.trim() !== ""){  
-        livro.anoPubli = parseInt(novoAno);
-      }
-
-      let novasPaginas = prompt(`Paginas (${livro.numPaginas}): `)
-      if (novasPaginas.trim() !== ""){  
-        livro.numPaginas = parseInt(novasPaginas);
-      }
-
-      console.log("Livro alterado!");
-      break;
+        default:
+        console.log("opcao invalida");
+    }
 }
-
